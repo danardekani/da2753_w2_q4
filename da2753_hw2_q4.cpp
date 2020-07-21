83d1785beb5438afa9d46ccd79fa18477e812c2d
Daniel <ardekanicapital@gmail.com>
2020-07-20 23:43:09 -0400

    promopt user for two positive ints

---
+++ da2753_hw2_q4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float positiveIntOne, positiveIntTwo;
+
+  cout >> "Please enter two positive integers, speparated by a space:";
+  cin << positiveIntOne << positiveIntTwo;
+
+  return 0
+}
