385677736e65226e591ac0c88a0f057863e657e0
Daniel <ardekanicapital@gmail.com>
2020-07-21 00:00:56 -0400

    add, subtract, mutiply, and ddivide two positive ints

--- da2753_hw2_q4.cpp
+++ da2753_hw2_q4.cpp
@@ -4,9 +4,20 @@ using namespace std;
 int main()
 {
   float positiveIntOne, positiveIntTwo;
+  float divValue, modValue;
 
-  cout >> "Please enter two positive integers, speparated by a space:";
-  cin << positiveIntOne << positiveIntTwo;
+  cout << "Please enter two positive integers, speparated by a space:";
+  cin >> positiveIntOne >> positiveIntTwo;
 
-  return 0
+  divValue = positiveIntOne / positiveIntTwo;
+  // modValue = positiveIntOne % positiveIntTwo;
+
+  cout << positiveIntOne + positiveIntTwo << endl;
+  cout << positiveIntOne - positiveIntTwo << endl;
+  cout << positiveIntOne * positiveIntTwo << endl;
+  cout << positiveIntOne / positiveIntTwo << endl;
+  cout << positiveIntOne << " div " << positiveIntTwo << " = " << divValue << endl;
+  // cout << positiveIntOne << " mod " << positiveIntTwo << "=" << modValue;
+
+  return 0;
 }
