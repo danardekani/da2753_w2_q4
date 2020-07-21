910747d5f5462f4acfeb09c16a358a951b30f5c5
Daniel <ardekanicapital@gmail.com>
2020-07-21 17:00:13 -0400

    update output statements.

--- da2753_hw2_q4.cpp
+++ da2753_hw2_q4.cpp
@@ -6,16 +6,16 @@ int main()
   int positiveIntOne, positiveIntTwo;
   int divValue, modValue;
 
-  cout << "Please enter two positive integers, speparated by a space:";
+  cout << "Please enter two positive integers, speparated by a space:" << endl;
   cin >> positiveIntOne >> positiveIntTwo;
 
   divValue = positiveIntOne / positiveIntTwo;
   modValue = positiveIntOne % positiveIntTwo;
 
-  cout << positiveIntOne + positiveIntTwo << endl;
-  cout << positiveIntOne - positiveIntTwo << endl;
-  cout << positiveIntOne * positiveIntTwo << endl;
-  cout << positiveIntOne / positiveIntTwo << endl;
+  cout << positiveIntOne << " + " << positiveIntTwo << " = " << positiveIntOne + positiveIntTwo << endl;
+  cout << positiveIntOne << " - " << positiveIntTwo << " = " << positiveIntOne - positiveIntTwo << endl;
+  cout << positiveIntOne << " * " << positiveIntTwo << " = " << positiveIntOne * positiveIntTwo << endl;
+  cout << positiveIntOne << " / " << positiveIntTwo << " = " << positiveIntOne / positiveIntTwo << endl;
   cout << positiveIntOne << " div " << positiveIntTwo << " = " << divValue << endl;
   cout << positiveIntOne << " mod " << positiveIntTwo << " = " << modValue << endl;
 
