9073c90322e6cdd942b00fb18e20a8868fa627cf
Daniel <ardekanicapital@gmail.com>
2020-07-21 00:07:42 -0400

    chat data type from float to int.

--- da2753_hw2_q4.cpp
+++ da2753_hw2_q4.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 int main()
 {
-  float positiveIntOne, positiveIntTwo;
-  float divValue, modValue;
+  int positiveIntOne, positiveIntTwo;
+  int divValue, modValue;
 
   cout << "Please enter two positive integers, speparated by a space:";
   cin >> positiveIntOne >> positiveIntTwo;
 
   divValue = positiveIntOne / positiveIntTwo;
-  // modValue = positiveIntOne % positiveIntTwo;
+  modValue = positiveIntOne % positiveIntTwo;
 
   cout << positiveIntOne + positiveIntTwo << endl;
   cout << positiveIntOne - positiveIntTwo << endl;
   cout << positiveIntOne * positiveIntTwo << endl;
   cout << positiveIntOne / positiveIntTwo << endl;
   cout << positiveIntOne << " div " << positiveIntTwo << " = " << divValue << endl;
-  // cout << positiveIntOne << " mod " << positiveIntTwo << "=" << modValue;
+  cout << positiveIntOne << " mod " << positiveIntTwo << " = " << modValue << endl;
 
   return 0;
 }
